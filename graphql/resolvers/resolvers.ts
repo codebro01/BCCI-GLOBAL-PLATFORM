4a75966ba6245796ef3b6c989c0edeff6c76acbb
codebro <samsondamilola.99@gmail.com>
2025-07-17 18:30:04 +0100

    changed goal resolver touppercase

--- graphql/resolvers/resolvers.ts
+++ graphql/resolvers/resolvers.ts
@@ -3,7 +3,7 @@ const { mergeResolvers } = require('@graphql-tools/merge')
 const { loadFilesSync } = require('@graphql-tools/load-files')
 const userResolver = require('@graphql/resolvers/User.resolver')
 const authResolver = require('@graphql/resolvers/Auth.resolver')
-const goalResolver = require('@graphql/resolvers/Goal.resolver')
+const goalResolver = require('@graphql/resolvers/goal.resolver')
 const eventResolver = require('@graphql/resolvers/Event.resolver')
 
 const resolvers = [userResolver, authResolver, goalResolver, eventResolver]
