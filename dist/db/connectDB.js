3ed9b3c073866dead4671b52bfc1f4c358647bef
codebro <samsondamilola.99@gmail.com>
2025-08-04 15:54:28 +0100

    added req.headers for mobile auth

--- dist/db/connectDB.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-const { connect } = require('mongoose');
-function connectDB(MONGO_URI) {
-    return connect(MONGO_URI);
-}
-exports.connectDB = connectDB;
-//# sourceMappingURL=connectDB.js.map
